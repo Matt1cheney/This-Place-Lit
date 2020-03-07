e0c9fd3e50a2bb6a4955867a200c4d7c23850c12
BytteL <lettybedard@gmail.com>
2020-03-07 13:05:32 -0500

    added doc dot ready

--- public/js/index.js
+++ public/js/index.js
@@ -13,23 +13,26 @@
 //   }
 // }
 
-//submit on res page is clicked
-$("#submit").on("click", function(){
-  
-  // grab form input
-  let newReservation = {
-    name: $('#personName').val().trim(),
-    phone: $('#phoneNumber').val().trim(),
-    email: $('#email').val().trim(),
-    uniqueID: $('#uniqueID').val().trim()
-  };
-  
-  console.log(newReservation);
-  
-  $.post("/api/tables", newReservation)
+$(document).ready(function(){
+  //submit on res page is clicked
+  $("#submit").on("click", function(){
+    
+    // grab form input
+    let newReservation = {
+      name: $('#personName').val().trim(),
+      phone: $('#phoneNumber').val().trim(),
+      email: $('#email').val().trim(),
+      uniqueID: $('#uniqueID').val().trim()
+    };
+    
+    console.log(newReservation);
+    
+    $.post("/api/tables", newReservation)
     .then(function(data) {
       console.log("reservations.html", data);
       alert("Adding reservation...");
     });
+  });
 });
-
+  
+  
